dc3a3899dd2b8268a24c04841386e7927f55d05c
Vardan <varnar@users.noreply.github.com>
2018-02-24 18:01:46 -0800

    Update nodejsRunner.js

--- nodejsRunner.js
+++ nodejsRunner.js
@@ -1,12 +1,3 @@
-#!/usr/bin/env sh
-
-
-#The folder which we mount on docker is named the usercode.
-#Move into the directory and execute the loop
-cd /temp/
-~/.sdkman/candidates/kotlin/current/bin/kotlinc file.kt -include-runtime -d file
-.jar && java -jar file.jar
-root@machine:/usercode# cat nodejsRunner.js
 var fs = require('fs')
 var vm = require('vm')
 
