696ccc70cb042398a4c9a2fa9e54f12faa694e5d
Vardan <varnar@users.noreply.github.com>
2018-02-24 17:47:40 -0800

    Update javaRunner.sh

--- javaRunner.sh
+++ javaRunner.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 
-################################################################################
-######
+######################################################################################
 #   This script is specifically made to handle the compilation of java
 #   The idea is to cater with the difference of class name(s) inside file
 #   and the file name itself when executing the java .class files
@@ -19,31 +18,24 @@
 #       The javap tool disassembles compiled Java™ files and prints
 #       out a representation of the Java program. This may be helpful
 #       when the original source code is no longer available on a system.
-#       source: publib.boulder.ibm.com/infocenter/iseries/v5r4/topic/rzaha/javap
-.htm
+#       source: publib.boulder.ibm.com/infocenter/iseries/v5r4/topic/rzaha/javap.htm
 #
-#   3. If the class contains the main function run the java command to run it an
-d exit
+#   3. If the class contains the main function run the java command to run it and exit
 #   4. This script only executes the first main function it finds
 #
 #   Inspired From: http://stackoverflow.com/a/8870863
 #
-################################################################################
-#######
-
+#######################################################################################
 
 #The folder which we mount on docker is named the usercode.
 #Move into the directory and execute the loop
 cd /temp/
 
-
-
 for classfile in *.class; do
     classname=${classfile%.*}
     #echo $classname
 
-    #Execute fgrep with -q option to not display anything on stdout when the mat
-ch is found
+    #Execute fgrep with -q option to not display anything on stdout when the match is found
     if javap -public $classname | fgrep -q 'public static void main(java.lang.St
 ring[])'; then
         java $classname "$@"
