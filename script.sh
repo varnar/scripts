aead504eee91990f6c56c87345c5ed329de09deb
Vardan <varnar@users.noreply.github.com>
2018-03-03 10:04:50 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,8 +5,7 @@
 #   - The script takes 3 arguments
 #       1. The compiler that is to compile the source file.
 #       2. The source file that is to be compiled/interpreted
-#       3. Additional argument only needed for compilers, to execute the object
-code
+#       3. Additional argument only needed for compilers, to execute the object code
 #
 #   - Sample execution command:   $: ./script.sh g++ file.cpp ./a.out
 #
@@ -26,19 +25,15 @@ addtionalArg=$4
 #
 #   - if third arguemtn is empty Branch 1 is followed. An interpretor was called
 #   - else Branch2 is followed, a compiler was invoked
-#   - In Branch2. We first check if the compile operation was a success (code re
-turned 0)
+#   - In Branch2. We first check if the compile operation was a success (code returned 0)
 #
-#   - If the return code from compile is 0 follow Branch2a and call the output c
-ommand
+#   - If the return code from compile is 0 follow Branch2a and call the output command
 #   - Else follow Branch2b and output error Message
 #
 #   - Stderr and Stdout are restored
 #   - Once the logfile is completely written, it is renamed to "completed"
-#   - The purpose of creating the "completed" file is because NodeJs searches fo
-r this file
-#   - Upon finding this file, the NodeJS Api returns its content to the browser
-and deletes the folder
+#   - The purpose of creating the "completed" file is because NodeJs searches for this file
+#   - Upon finding this file, the NodeJS Api returns its content to the browserand deletes the folder
 #
 #
 ########################################################################
